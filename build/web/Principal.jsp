d341480eeb82b261cf4b1f47a37e286e2502750f
sammy <sammy.kazzu@outlook.com>
2021-05-20 22:25:27 -0600

    Cambios
    CrearMuestra

--- build/web/Principal.jsp
+++ build/web/Principal.jsp
@@ -9,8 +9,11 @@
             <div class="carousel-item">      
                 <img src="img/Slider/home-slider-2.png" class="d-block w-100" alt=""/>
             </div>
-            <div class="carousel-item">      
-                <img src="img/Slider/slider-principal.jpg" class="d-block w-100" alt=""/>
+            <div class="carousel-item">                
+                <img src="img/Slider/slider-1.jpg" class="d-block w-100" alt=""/>
+            </div>
+            <div class="carousel-item">                
+                <img src="img/Slider/slider11.jpg" class="d-block w-100" alt=""/>
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -21,6 +24,7 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
-    </div>    
+    </div>
+    
 </div>
 <%@include file="footer.jsp" %>
--- src/java/API/Conection.java
+++ src/java/API/Conection.java
@@ -1,7 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package API;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
 /**
  *
  * @author SammyKazzu
@@ -10,16 +16,18 @@ public class Conection {
     
     Connection con;
     String drive = "com.mysql.cj.jdbc.Driver";    
-    String url = "jdbc:mysql://localhost:3308/sbo_laboratoriolb_2021?serverTimezone=UTC";
+    String url = "jdbc:mysql://localhost:3308/sbo_labendicion_2021?serverTimezone=UTC";
     String user = "root";
     String pass="";
     
-    public Connection getConnection(){        
+    public Connection getConnection(){
+        
         try{
             Class.forName(drive);
             con = DriverManager.getConnection(url, user, pass);
         }catch(Exception e){            
         }
         return con;
-    }    
+    }
+    
 }
