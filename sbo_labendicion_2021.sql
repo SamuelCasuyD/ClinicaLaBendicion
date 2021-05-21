e1895d64ca4caed1026860bc1ce83d228a42f34a
sammy <sammy.kazzu@outlook.com>
2021-05-20 20:00:15 -0600

    Update base de datos
    agregar tabla expedientes

--- sbo_labendicion_2021.sql
+++ sbo_labendicion_2021.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3308
--- Tiempo de generación: 20-05-2021 a las 14:21:03
+-- Tiempo de generación: 21-05-2021 a las 01:53:42
 -- Versión del servidor: 8.0.18
 -- Versión de PHP: 7.3.12
 
@@ -25,6 +25,59 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `expedientes`
+--
+
+DROP TABLE IF EXISTS `expedientes`;
+CREATE TABLE IF NOT EXISTS `expedientes` (
+  `idexpediente` int(11) NOT NULL AUTO_INCREMENT,
+  `NoExpediente` varchar(30) NOT NULL,
+  `Dpi` varchar(15) NOT NULL,
+  `PrimerNombre` varchar(50) NOT NULL,
+  `SegundoNombre` varchar(50) DEFAULT NULL,
+  `OtroNombre` varchar(50) DEFAULT NULL,
+  `PrimerApellido` varchar(50) NOT NULL,
+  `SegundoApellido` varchar(50) DEFAULT NULL,
+  `FechaNacimiento` varchar(12) NOT NULL,
+  `TelDomicilio` varchar(15) DEFAULT NULL,
+  `TelMovil` varchar(15) DEFAULT NULL,
+  `Eamil` varchar(75) DEFAULT NULL,
+  `Nit` varchar(10) DEFAULT NULL,
+  `FechaCreacion` varchar(25) NOT NULL,
+  `FechaModificacion` varchar(25) DEFAULT NULL,
+  `UsuaroiCreacion` int(11) NOT NULL,
+  `UsuarioModificaion` int(11) DEFAULT NULL,
+  `numeroFactura` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`idexpediente`),
+  UNIQUE KEY `NoExpediente` (`NoExpediente`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `expedientes`
+--
+
+INSERT INTO `expedientes` (`idexpediente`, `NoExpediente`, `Dpi`, `PrimerNombre`, `SegundoNombre`, `OtroNombre`, `PrimerApellido`, `SegundoApellido`, `FechaNacimiento`, `TelDomicilio`, `TelMovil`, `Eamil`, `Nit`, `FechaCreacion`, `FechaModificacion`, `UsuaroiCreacion`, `UsuarioModificaion`, `numeroFactura`) VALUES
+(1, '2021-04-27-01-0000001', '2983312054875', 'Douglas', 'Alexander', NULL, 'Sacalxot', 'Elias', '01-05-1996', '25547865', '32243548', 'd.alexelias@gmail.com', '94754878', '31-03-2021 17:06:42', NULL, 10, NULL, '12345678910'),
+(2, '2021-05-08-02-0000002', '2052853760101', 'Hector ', 'Santiago', NULL, 'Casuy', 'Rodriguez', '01-05-1995', '24226120', '30213865', 'test@info.com', NULL, '', NULL, 0, NULL, NULL);
+
+--
+-- Disparadores `expedientes`
+--
+DROP TRIGGER IF EXISTS `tg_NoExpediente`;
+DELIMITER $$
+CREATE TRIGGER `tg_NoExpediente` BEFORE INSERT ON `expedientes` FOR EACH ROW BEGIN
+DECLARE correlativo int;
+SET correlativo =(SELECT ifnull(MAX(CONVERT(substring(idexpediente,1), signed integer)),0) FROM expedientes) +1;
+
+set new.NoExpediente = concat(year(now()),'-',lpad(MONTH (NOW()),2,'0'),"-",lpad(DAY(NOW()),2,'0'),"-",lpad(correlativo,2,"0"),"-",lpad(correlativo,7,"0"));
+    
+end
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `muestra`
 --
 
@@ -40,6 +93,7 @@ CREATE TABLE IF NOT EXISTS `muestra` (
   `FechaCreacion` datetime DEFAULT NULL,
   `FechaModificacion` datetime DEFAULT NULL,
   `Eliminado` tinyint(1) DEFAULT NULL,
+  `idSolicitudes` int(11) DEFAULT NULL,
   PRIMARY KEY (`IdMuestra`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -47,23 +101,23 @@ CREATE TABLE IF NOT EXISTS `muestra` (
 -- Volcado de datos para la tabla `muestra`
 --
 
-INSERT INTO `muestra` (`IdMuestra`, `NumMuestra`, `IdTipoMuestra`, `Presentacion`, `CantidadUnidades`, `IdUnidadMedida`, `Adjunto`, `FechaCreacion`, `FechaModificacion`, `Eliminado`) VALUES
-(1, 'AAAAA-AA-01-000001', 1, 'Somos bananos 3333333', 1, 1, 'trrr', '2021-05-08 00:00:00', '2021-05-15 00:00:00', 0),
-(2, 'AAAAAA2-02-000002', 2, 'msta', 2, 2, 'ttrterert', '2021-05-08 00:00:00', NULL, 0),
-(3, 'AAAAA-A3-03-000003', 1, 'temerarios', 3, 1, 'trest', '2021-05-08 00:00:00', NULL, 0),
-(4, 'ABCD4-A4-04-000004', 3, 'Muestras de orina ', 4, 1, '/test/res', '2021-05-08 00:00:00', '2021-05-15 00:00:00', 1),
-(5, 'ABCD5-A5-05-000005', 4, 'tetser', 2255, 3, NULL, '2021-05-02 00:00:00', NULL, 1),
-(6, 'ABCD6-A6-06-000006', 2, 'Amores', 8989, 2, NULL, '2021-05-01 00:00:00', NULL, 1),
-(7, 'ABCD7-A7-07-000007', 1, 'Muestra de Sangre', 12, 1, NULL, '2021-05-01 00:00:00', NULL, 1),
-(8, 'ABCD8-A8-08-000008', 2, 'De la abuela coco', 2, 2, NULL, '2021-05-01 00:00:00', '2021-05-15 00:00:00', 0),
-(9, 'ABCD9-A9-09-000009', 2, 'fdfdf', 7878, 1, NULL, '2021-05-15 00:00:00', NULL, 0),
-(10, 'ABC10-10-10-000010', 1, 'ttetet   eerere ererere', 100, 1, NULL, '2021-05-15 00:00:00', NULL, 0),
-(11, 'ABC11-11-11-000011', 4, 'Color amarillento', 2133, 1, NULL, '2021-05-15 00:00:00', NULL, 0),
-(12, 'ABC12-12-12-000012', 1, 'Ormonas', 8555, 2, NULL, '2021-05-15 00:00:00', NULL, 0),
-(13, 'ABC13-13-13-000013', 3, 'Color negro', 78, 2, NULL, '2021-05-15 00:00:00', NULL, 0),
-(14, 'ABC14-14-14-000014', 2, 'tres Tres', 12, 2, NULL, '2021-05-15 00:00:00', '2021-05-19 00:00:00', 0),
-(15, 'ABC15-15-15-000015', 1, 'Muestras de Sangre', 8565, 2, NULL, '2021-05-15 00:00:00', '2021-05-19 00:00:00', 0),
-(16, 'ABC16-16-16-000016', 4, 'Test liquido', 1, 1, NULL, '2021-05-19 00:00:00', NULL, 0);
+INSERT INTO `muestra` (`IdMuestra`, `NumMuestra`, `IdTipoMuestra`, `Presentacion`, `CantidadUnidades`, `IdUnidadMedida`, `Adjunto`, `FechaCreacion`, `FechaModificacion`, `Eliminado`, `idSolicitudes`) VALUES
+(1, 'AAAAA-AA-01-000001', 1, 'Somos bananos 3333333', 1, 1, 'trrr', '2021-05-08 00:00:00', '2021-05-15 00:00:00', 0, NULL),
+(2, 'AAAAAA2-02-000002', 2, 'msta', 2, 2, 'ttrterert', '2021-05-08 00:00:00', NULL, 0, NULL),
+(3, 'AAAAA-A3-03-000003', 1, 'temerarios', 3, 1, 'trest', '2021-05-08 00:00:00', NULL, 0, NULL),
+(4, 'ABCD4-A4-04-000004', 3, 'Muestras de orina ', 4, 1, '/test/res', '2021-05-08 00:00:00', '2021-05-15 00:00:00', 1, NULL),
+(5, 'ABCD5-A5-05-000005', 4, 'tetser', 2255, 3, NULL, '2021-05-02 00:00:00', NULL, 1, NULL),
+(6, 'ABCD6-A6-06-000006', 2, 'Amores', 8989, 2, NULL, '2021-05-01 00:00:00', NULL, 1, NULL),
+(7, 'ABCD7-A7-07-000007', 1, 'Muestra de Sangre', 12, 1, NULL, '2021-05-01 00:00:00', NULL, 1, NULL),
+(8, 'ABCD8-A8-08-000008', 2, 'De la abuela coco', 2, 2, NULL, '2021-05-01 00:00:00', '2021-05-15 00:00:00', 0, NULL),
+(9, 'ABCD9-A9-09-000009', 2, 'fdfdf', 7878, 1, NULL, '2021-05-15 00:00:00', NULL, 0, NULL),
+(10, 'ABC10-10-10-000010', 1, 'ttetet   eerere ererere', 100, 1, NULL, '2021-05-15 00:00:00', NULL, 0, NULL),
+(11, 'ABC11-11-11-000011', 4, 'Color amarillento', 2133, 1, NULL, '2021-05-15 00:00:00', NULL, 0, NULL),
+(12, 'ABC12-12-12-000012', 1, 'Ormonas', 8555, 2, NULL, '2021-05-15 00:00:00', NULL, 0, NULL),
+(13, 'ABC13-13-13-000013', 3, 'Color negro', 78, 2, NULL, '2021-05-15 00:00:00', NULL, 0, NULL),
+(14, 'ABC14-14-14-000014', 2, 'tres Tres', 12, 2, NULL, '2021-05-15 00:00:00', '2021-05-19 00:00:00', 0, NULL),
+(15, 'ABC15-15-15-000015', 1, 'Muestras de Sangre', 8565, 2, NULL, '2021-05-15 00:00:00', '2021-05-19 00:00:00', 0, NULL),
+(16, 'ABC16-16-16-000016', 4, 'Test liquido', 1, 1, NULL, '2021-05-19 00:00:00', NULL, 0, NULL);
 
 --
 -- Disparadores `muestra`
@@ -107,6 +161,48 @@ CREATE TABLE IF NOT EXISTS `rollusuario` (
 INSERT INTO `rollusuario` (`IdRoll`, `NombreRoll`, `FechaCreacion`, `Eliminado`) VALUES
 (1, 'Cliente', '2021-05-15 00:00:00', 0),
 (2, 'Administrador', '2021-05-15 00:00:00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitudes_medicas`
+--
+
+DROP TABLE IF EXISTS `solicitudes_medicas`;
+CREATE TABLE IF NOT EXISTS `solicitudes_medicas` (
+  `idSolicitudes` int(11) NOT NULL AUTO_INCREMENT,
+  `codigoSolicitud` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `tipoSolicitante` int(4) NOT NULL,
+  `tipoSolicitud` int(11) NOT NULL,
+  `Descripcion` varchar(2000) NOT NULL,
+  `NoExpediente` varchar(30) NOT NULL,
+  `nit` varchar(10) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `Telefono` varchar(15) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `FechaCreacion` varchar(25) NOT NULL,
+  `FechaModificacion` varchar(25) DEFAULT NULL,
+  `UsuarioCreacion` int(11) NOT NULL,
+  `UsuarioModificacion` int(11) DEFAULT NULL,
+  `UsuarioAsignacion` int(11) DEFAULT NULL,
+  `tipoSoporte` int(2) NOT NULL,
+  `numSoporte` varchar(50) NOT NULL,
+  `estado_solicitud` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idSolicitudes`),
+  KEY `tipoSolicitante` (`tipoSolicitante`),
+  KEY `tipoSolicitud` (`tipoSolicitud`),
+  KEY `estado_solicitud` (`estado_solicitud`),
+  KEY `NoExpediente` (`NoExpediente`),
+  KEY `tipoSoporte` (`tipoSoporte`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `solicitudes_medicas`
+--
+
+INSERT INTO `solicitudes_medicas` (`idSolicitudes`, `codigoSolicitud`, `tipoSolicitante`, `tipoSolicitud`, `Descripcion`, `NoExpediente`, `nit`, `nombre`, `Telefono`, `email`, `FechaCreacion`, `FechaModificacion`, `UsuarioCreacion`, `UsuarioModificacion`, `UsuarioAsignacion`, `tipoSoporte`, `numSoporte`, `estado_solicitud`) VALUES
+(1, 'EX-20210507-00001', 20, 1, 'Solotrajounasolicitudmedica', '2021-05-03-02-0000002', '45434567', 'Katherine Castellanos', '54505853', 'ejemplo@gmail.com', '07-05-2021', NULL, 2, NULL, NULL, 1, '10000AAAAAAAAAA', 1),
+(2, 'IN-20210507-00002', 10, 2, 'Faltamuestradeheces', '2021-05-03-02-0000002', '45434567', 'Katherine Castellanos', '45685245', 'ktha@gmail.com', '07-05-2021', NULL, 2, NULL, NULL, 1, '10000AAAAAAAAAA', 1);
 
 -- --------------------------------------------------------
 
